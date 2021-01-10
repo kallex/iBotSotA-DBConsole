7bcb7ce96450984e6f0e4a077cf542da9451e7b1
Kalle Launiala <kalle.launiala@protonit.net>
2021-01-10 21:15:11 +0200

    Initial solution setup

---
+++ src/TimeseriesConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TimeseriesConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
