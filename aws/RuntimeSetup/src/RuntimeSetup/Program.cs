460685e52ea4edb1b5244c0b84fcc7747379506f
Kalle Launiala <kalle.launiala@protonit.net>
2021-02-21 23:22:14 +0200

    Initial separated structure of Infra and Runtime

--- aws/RuntimeSetup/src/RuntimeSetup/Program.cs
+++ aws/RuntimeSetup/src/RuntimeSetup/Program.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Environment = Amazon.CDK.Environment;
 
 namespace RuntimeSetup
 {
@@ -10,8 +11,24 @@ namespace RuntimeSetup
         public static void Main(string[] args)
         {
             var app = new App();
-            new RuntimeSetupStack(app, "RuntimeSetupStack");
+            Environment env = getEnvironment();
+            StackProps stackProps = new StackProps
+            {
+                Env = env
+            };
+            var appName = "ibotsota";
+            new RuntimeSetupStack(app, "DevRuntime", new EnvironmentDetails() { AppPrefix = appName, EnvSuffix = "dev", Type = EnvironmentType.Dev }, stackProps);
+            new RuntimeSetupStack(app, "TestRuntime", new EnvironmentDetails() { AppPrefix = appName, EnvSuffix = "test", Type = EnvironmentType.Test }, stackProps);
+            new RuntimeSetupStack(app, "BetaRuntime", new EnvironmentDetails() { AppPrefix = appName, EnvSuffix = "beta", Type = EnvironmentType.Beta }, stackProps);
             app.Synth();
         }
+        private static Environment getEnvironment()
+        {
+            return new Environment()
+            {
+                Region = "eu-west-1"
+            };
+        }
+
     }
 }
