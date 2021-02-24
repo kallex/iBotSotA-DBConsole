baa0f92c6590bd74a75258c76f65234dba7fb0fb
Kalle Launiala <kalle.launiala@protonit.net>
2021-02-25 00:32:27 +0200

    WIP: Steam integration

---
+++ src/DataServiceCore/ISteamService.cs
@@ -0,0 +1,7 @@
+﻿namespace DataServiceCore
+{
+    public interface ISteamService
+    {
+
+    }
+}
---
+++ src/SteamService/SteamService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using DataServiceCore;
+using Steamworks;
+
+namespace SteamService
+{
+    public class SteamService : ISteamService
+    {
+        public SteamService()
+        {
+            SteamClient.Init(1518060);
+            var steamId = SteamClient.SteamId;
+        }
+
+
+        public void ValidateToken()
+        {
+        }
+    }
+}
