1a0f07a3ac6a2d6e94bf5c02318df0b96a1256c5
Kalle Launiala <kalle.launiala@protonit.net>
2021-03-07 09:08:36 +0200

    Cleaned up lots of test code

--- src/iBotSotALambda.Tests/DataServiceControllerTests.cs
+++ src/iBotSotALambda.Tests/DataServiceControllerTests.cs
@@ -36,7 +36,6 @@ namespace iBotSotALambda.Tests
 
                 var authData = await steamService.GetAuthTokenA();
                 var authenticated = await steamService.ValidateAuthToken(authData.steamIdValue, authData.authToken);
-                var dataServiceController = new DataServiceController();
                 Assert.True(authenticated);
             }
             catch (Exception ex)
@@ -76,13 +75,14 @@ namespace iBotSotALambda.Tests
             var controller = new DataServiceController();
             var authDataHex = authData.authToken.ToHexString();
             //var result = (JsonResult) await controller.AuthTest(authData.steamIdValue, authDataHex);
-            var result = (JsonResult) await controller.AuthTest(authDataHex);
+            var result = (JsonResult) await controller.GetSteamAuthentication(authDataHex);
             var expected = new JsonResult(new
             {
                 statusMessage = "OK",
                 authenticated = true
             });
-            Assert.Equal(expected.Value, result.Value);
+            var content = result.ToString();
+            Assert.StartsWith("{\"statusMessage\":\"OK\",\"authenticated\":true,\"steamid\":", content);
         }
 
         [Fact]
@@ -104,27 +104,8 @@ namespace iBotSotALambda.Tests
             var url = $"https://partner.steam-api.com/ISteamUserAuth/AuthenticateUserTicket/v1/?key={SteamWebApiKey}&appid={SteamAppId}&ticket={authDataHex}";
             var response = await httpClient.GetAsync(url);
             var content = await response.Content.ReadAsStringAsync();
-        }
-
-        [Fact]
-        public async Task DevServerAuthenticateTest()
-        {
-            var container = new Container();
-            container.Register<ISteamService, SteamService.SteamService>(Reuse.Singleton);
-
-            var steamService = container.Resolve<ISteamService>();
-            steamService.InitService(SteamAppId, SteamWebApiKey);
-
-            steamService.InitSteamClient();
-            var authData = await steamService.GetAuthTokenA();
-
-            using var httpClient = new HttpClient();
-            var authDataHex = authData.authToken.ToHexString();
-            var url = $"{LambdaEndpointUrl}/api/DataService/AuthTestString?authDataHex={authDataHex}";
-            var response = await httpClient.GetAsync(url);
-            var content = await response.Content.ReadAsStringAsync();
-            Assert.StartsWith("Authenticated: True", content);
-            Assert.EndsWith("Status: OK", content);
+            Assert.StartsWith("{\"response\":{\"params\":{\"result\":\"OK\",", content);
+            Assert.EndsWith("\",\"vacbanned\":false,\"publisherbanned\":false}}}", content);
         }
 
         [Fact]
@@ -141,11 +122,10 @@ namespace iBotSotALambda.Tests
 
             using var httpClient = new HttpClient();
             var authDataHex = authData.authToken.ToHexString();
-            var url = $"{LambdaEndpointUrl}/api/DataService/AuthTest?authDataHex={authDataHex}";
+            var url = $"{LambdaEndpointUrl}/api/DataService/GetSteamAuthentication?authDataHex={authDataHex}";
             var response = await httpClient.GetAsync(url);
             var content = await response.Content.ReadAsStringAsync();
-            Assert.StartsWith("Authenticated: True", content);
-            Assert.EndsWith("Status: OK", content);
+            Assert.StartsWith("{\"statusMessage\":\"OK\",\"authenticated\":true,\"steamid\":", content);
         }
 
 
--- src/iBotSotALambda/Controllers/DataServiceController.cs
+++ src/iBotSotALambda/Controllers/DataServiceController.cs
@@ -18,7 +18,7 @@ namespace iBotSotALambda.Controllers
         public Container Container { get; set; }
 
         [HttpPost]
-        public void SubmitMatchData([FromBody] byte[] matchData)
+        public void SubmitMatchData([FromQuery] string authDataHex, [FromBody] byte[] matchData)
         {
             //var container = new Container();
             //container.Register<ISteamService, SteamService.SteamService>(Reuse.Singleton);
@@ -28,166 +28,30 @@ namespace iBotSotALambda.Controllers
             //var authToken = steamService.GetAuthTokenA();
         }
 
-        [HttpGet("{arg}")]
-        public string TestGet(int arg)
-        {
-            var now = DateTime.Now;
-            return $"Now: {now} Arg: {arg}";
-        }
-
-        [HttpGet("{arg}")]
-        public async Task<string> TestXGetAsync(int arg)
-        {
-            var now = DateTime.Now;
-            return $"Now: {now} Arg: {arg}";
-        }
-
-
-        [HttpGet("{arg}")]
-        public async Task TestGetAsyncStream(int arg)
-        {
-            var now = DateTime.Now;
-            var result = $"{nameof(TestGetAsyncStream)} Now: {now} Arg: {arg}";
-            var data = Encoding.UTF8.GetBytes(result);
-            await Response.Body.WriteAsync(data);
-            //Response.StatusCode = 200;
-        }
-
-
-        [HttpGet("{arg}")]
-        public async Task<string> DiagTest(string arg)
-        {
-            string statusMessage = "";
-            try
-            {
-                /*
-                var rules = DryIoc.Rules.Default
-                    .With(DryIoc.FactoryMethod.ConstructorWithResolvableArguments)
-                    .WithFactorySelector(DryIoc.Rules.SelectLastRegisteredFactory())
-                    .WithTrackingDisposableTransients();
-
-                this.Container = new DryIoc.Container(rules);
-                Container.Register<ISteamService, SteamService.SteamService>(Reuse.Singleton);
-
-
-                var steamService = Container.Resolve<ISteamService>();
-                steamService.InitService(Startup.SteamAppId, Startup.SteamWebApiKey);
-                authenticated = await steamService.ValidateAuthTokenWeb(arg);
-                statusMessage = "OK";
-                */
-                if (!arg.StartsWith("nop"))
-                {
-                    using var httpClient = new HttpClient();
-                    var fullUrl = $"https://{arg}";
-                    var response = await httpClient.GetAsync(fullUrl);
-                    var statusCode = response.StatusCode;
-                    statusMessage = $"Response status for {fullUrl} was {statusCode}";
-                }
-            }
-            catch (Exception ex)
-            {
-                statusMessage = ex.ToString();
-            }
-
-            var result = $"Diagtest {DateTime.Now}: Status: {statusMessage}";
-            return result;
-        }
-
-
-        [HttpGet("{authDataHex}")]
-        public async Task<string> AuthTestNamedPartial(string authDataHex)
-        {
-            string statusMessage = "";
-            bool authenticated = false;
-            try
-            {
-                var rules = DryIoc.Rules.Default
-                    .With(DryIoc.FactoryMethod.ConstructorWithResolvableArguments)
-                    .WithFactorySelector(DryIoc.Rules.SelectLastRegisteredFactory())
-                    .WithTrackingDisposableTransients();
-
-                this.Container = new DryIoc.Container(rules);
-                Container.Register<ISteamService, SteamService.SteamService>(Reuse.Singleton);
-
-                var steamService = Container.Resolve<ISteamService>();
-                //var steamService = (ISteamService) new SteamService.SteamService();
-                steamService.InitService(Startup.SteamAppId, Startup.SteamWebApiKey);
-                //authenticated = await steamService.ValidateAuthTokenWeb(authDataHex);
-                statusMessage = "OK (not)";
-            }
-            catch (Exception ex)
-            {
-                statusMessage = ex.ToString();
-            }
-
-            var result = $"AuthData: {authDataHex} Authenticated: {authenticated} - Status: {statusMessage}";
-            return result;
-        }
-
-
 
         [HttpGet]
-        public async Task<string> AuthTestString(string authDataHex)
+        public async Task<ActionResult> GetSteamAuthentication([FromQuery] string authDataHex)
         {
-            string statusMessage = "";
-            (bool authenticated, ulong steamid, ulong ownersteamid, bool vacbanned, bool publishedbanned) authenticated = default;
-            try
-            {
-                var rules = DryIoc.Rules.Default
-                    .With(DryIoc.FactoryMethod.ConstructorWithResolvableArguments)
-                    .WithFactorySelector(DryIoc.Rules.SelectLastRegisteredFactory())
-                    .WithTrackingDisposableTransients();
+            var rules = DryIoc.Rules.Default
+                .With(DryIoc.FactoryMethod.ConstructorWithResolvableArguments)
+                .WithFactorySelector(DryIoc.Rules.SelectLastRegisteredFactory())
+                .WithTrackingDisposableTransients();
 
-                this.Container = new DryIoc.Container(rules);
-                Container.Register<ISteamService, SteamService.SteamService>(Reuse.Singleton);
+            this.Container = new DryIoc.Container(rules);
+            Container.Register<ISteamService, SteamService.SteamService>(Reuse.Singleton);
 
 
-                var steamService = Container.Resolve<ISteamService>();
-                steamService.InitService(Startup.SteamAppId, Startup.SteamWebApiKey);
-                authenticated = await steamService.ValidateAuthTokenWeb(authDataHex);
-                statusMessage = "OK";
-            }
-            catch (Exception ex)
-            {
-                statusMessage = ex.ToString();
-            }
-
-            var result = $"Authenticated: {authenticated.authenticated} - SteamID: {authenticated.steamid} - Status: {statusMessage}";
-            return result;
-        }
-
-
-        [HttpGet]
-        public async Task<ActionResult> AuthTest([FromQuery] string authDataHex)
-        {
-            string statusMessage = "";
-            (bool authenticated, ulong steamid, ulong ownersteamid, bool vacbanned, bool publishedbanned) authenticated = default;
-            try
-            {
-                var rules = DryIoc.Rules.Default
-                    .With(DryIoc.FactoryMethod.ConstructorWithResolvableArguments)
-                    .WithFactorySelector(DryIoc.Rules.SelectLastRegisteredFactory())
-                    .WithTrackingDisposableTransients();
-
-                this.Container = new DryIoc.Container(rules);
-                Container.Register<ISteamService, SteamService.SteamService>(Reuse.Singleton);
-
-
-                var steamService = Container.Resolve<ISteamService>();
-                steamService.InitService(Startup.SteamAppId, Startup.SteamWebApiKey);
-                authenticated = await steamService.ValidateAuthTokenWeb(authDataHex);
-                statusMessage = "OK";
-            }
-            catch (Exception ex)
-            {
-                statusMessage = ex.ToString();
-            }
+            var steamService = Container.Resolve<ISteamService>();
+            steamService.InitService(Startup.SteamAppId, Startup.SteamWebApiKey);
+            var authResult = await steamService.ValidateAuthTokenWeb(authDataHex);
 
             return new JsonResult(new
             {
-                statusMessage = statusMessage,
-                authenticated = authenticated.authenticated,
-                steamid = authenticated.steamid
+                authResult.isAuthenticated,
+                authResult.steamId,
+                authResult.ownerSteamId,
+                authResult.vacBanned,
+                authResult.publisherBanned
             });
         }
     }
